f1e7a6442c0deace25f6811333ab56149eb7ca22
hunter-graves <hgraves@mail.sfsu.edu>
2020-03-13 12:53:00 -0700

    imported the Alert component to throw an alert and inform the user what valid input is. Changed conditional logic to use the isNaN function on the chosenNumber that the user input.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import {
+    View, 
+    Text, 
+    StyleSheet, 
+    Button, 
+    TouchableWithoutFeedback, 
+    Keyboard,
+    Alert
+ } from 'react-native';
+
 import Card from '../components/Card';
 import Colors from '../constants/colors';
 import Input from '../components/Input';
@@ -21,7 +30,12 @@ const StartGameScreen = props => {
 
     const confirmInputHandler = () => {
         const chosenNumber = parseInt(enteredValue);
-        if (chosenNumber === NaN || chosenNumber <=0 || chosenNumber > 99){
+        if (isNaN(chosenNumber) || chosenNumber <=0 || chosenNumber > 99){
+            Alert.alert(
+            'Invalid number!', 
+            'Number has to be a number between 1 and 99', 
+            [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+            );
             return;
         }
         setConfirmedState(true);
